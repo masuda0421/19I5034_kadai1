25eaeee18e50ec7196f84b6dda933ed6ee79e670
masuda0421 <82562239+masuda0421@users.noreply.github.com>
2021-05-10 02:55:10 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,113 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        # . . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        # # . # #
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+// Bボタンを押したら悲しい顔
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . # . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        # . . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . # . # .
+        . . . . .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+// Bボタンを押したら悲しい顔
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+// Bボタンを押したら悲しい顔
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
